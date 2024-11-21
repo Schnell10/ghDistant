1b1a2a0d0f5a623c8627a74668fad3426b98f92d
Pierre Schnell <pierreschnell@hotmail.com>
2024-11-21 15:54:05 +0100

    C2: extraction des constant dans un fichier à part

---
+++ constants.js
@@ -0,0 +1,3 @@
+export const TITLE_TEXT = "Welcome";
+export const BUTTON_TEXT = "Click Me";
+export const BACKGROUND_COLOR = "lightgrey";
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-document.getElementById("clickButton").addEventListener("click", () => {
-    alert("Button Clicked!");
-});
+import { TITLE_TEXT, BUTTON_TEXT } from './constants.js';
+
+document.querySelector("h1").innerText = TITLE_TEXT;
+document.getElementById("mainButton").innerText = BUTTON_TEXT;
