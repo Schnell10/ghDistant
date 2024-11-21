6a85a64fb6793dabc5e766fabd7550cdb971fc49
Pierre Schnell <pierreschnell@hotmail.com>
2024-11-21 15:55:35 +0100

    C3: mise à jorus taille et constante

--- constants.js
+++ constants.js
@@ -1,3 +1,3 @@
-export const TITLE_TEXT = "Welcome";
-export const BUTTON_TEXT = "Click Me";
+export const TITLE_TEXT = "Welcome to My App";
+export const BUTTON_TEXT = "Press Me!";
 export const BACKGROUND_COLOR = "lightgrey";
